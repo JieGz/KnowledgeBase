26f6df78353578da1373c9a88c4e840df10d44ea
jieguangzhi <jieguangzhi@joyy.sg>
2022-08-20 20:44:04 +0800

    feat: Initialize Project

---
+++ KnowledgeOfLogAgent/src/main/java/org/example/LogAgent.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author jieguangzhi
+ * @date 2022-08-20
+ */
+public class LogAgent {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ KnowledgeOfSpringBoot/src/main/java/com/knowledge/base/Application.java
@@ -0,0 +1,16 @@
+package com.knowledge.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jieguangzhi
+ * @date 2022-08-20
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
